d1ed4f35a809c067c4f884e723516be5d415d92b
local <di.grebenev@outlook.com>
2022-03-01 11:55:45 +0300

    commit

--- SF/SeaBattle/SeaBattle.py
+++ SF/SeaBattle/SeaBattle.py
@@ -1,2 +1,26 @@
 class GameDesk():
-    print('Игровое поле')
+    descript = 'игровое поле'
+    ls = []
+
+    def __init__(self, desc):
+        self.descript = desc
+        print(self.descript)
+        self.ls = [[0, 0, 0, 0, 0, 0],
+                   [0, 0, 0, 0, 0, 0],
+                   [0, 0, 0, 0, 0, 0],
+                   [0, 0, 0, 0, 0, 0],
+                   [0, 0, 0, 0, 0, 0],
+                   [0, 0, 0, 0, 0, 0]]
+        print(*self.ls, sep='\n')
+
+
+class Ship():
+    coord_start = (0, 0)
+    direction = 'horizontal'
+    def __init__(self):
+        print('ввести координату первой клетки коробля, его длинну и направление')
+
+    def
+
+
+
