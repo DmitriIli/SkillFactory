ffca4b65e114475e2c4834d0e5bb891df419c810
DmitriIli <di.grebenev@gmail.com>
2022-03-03 05:54:02 +0300

    add class Player

--- SF/SeaBattle/SeaBattle.py
+++ SF/SeaBattle/SeaBattle.py
@@ -1,35 +1,62 @@
-class GameDesk():
-    descript = 'игровое поле'
-    ls = []
+class GameDesk:
+    description = 'игровое поле'
+    __pole = []
 
     def __init__(self, desc):
-        self.descript = desc
-        print(self.descript)
-        self.ls = [[0, 0, 0, 0, 0, 0],
-                   [0, 0, 0, 0, 0, 0],
-                   [0, 0, 0, 0, 0, 0],
-                   [0, 0, 0, 0, 0, 0],
-                   [0, 0, 0, 0, 0, 0],
-                   [0, 0, 0, 0, 0, 0]]
-        print(*self.ls, sep='\n')
+        self.description = desc
+        print(self.description)
+        self.__pole = [[0 for j in range(6)] for i in range(6)]
+
+    def print_pole(self):
+        count = 1
+        print(' ', *(1, 2, 3, 4, 5, 6), sep='|')
+        for line in self.__pole:
+            print(count, *line, sep='|')
+            count += 1
+    # def add_ship(self, ):
 
 
-class Ship():
-    coord_start = (0, 0)
-    length = 0
+class Ship:
+    __coord_start = (0, 0)
+    __length = 0
+
+    def __init__(self, c_value, l_value):
+        self.__coord_start = c_value
+        self.__length = l_value
 
     @property
     def coord_start(self):
-        return self.coord_start
+        return self.__coord_start
 
     @coord_start.setter
     def coord_start(self, value):
-        self.coord_start = value
+        self.__coord_start = value
 
     @property
     def length(self):
-        return self.length
+        return self.__length
 
     @length.setter
     def length(self, value):
-        return self.length
+        self.__length = value
+
+
+class Player:
+    name = ''
+    list_of_move = []
+    desk = []
+
+    def __init__(self, name):
+        self.name = name
+        self.desk = GameDesk(self.name)
+
+    def make_move(self):
+        while 1:
+            print('ввести координаты (x,y) от 1 до 6 через пробел: ')
+            try:
+                coor_input = input().split()[:2]
+            except
+
+
+player1 = Player('игрок')
+player1.desk.print_pole()
